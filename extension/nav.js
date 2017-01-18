553a1ab79788e5d08b971dbee42e665f506ae51a
Bill Waters <wjwaters2@gaic.com>
2017-01-17 19:45:12 -0500

    Update nav.js

--- extension/nav.js
+++ extension/nav.js
@@ -1,3 +1,3 @@
 chrome.tabs.query( { active: true, currentWindow: true }, function( tabs ) {
-  chrome.tabs.update( tabs[0].id, { url: "http://www.dirtydiscoradio.com/wp-content/uploads/2014/10/tumblr_mqe93br8Er1r5nowgo1_1280.jpg" } ); 
+  chrome.tabs.update( tabs[0].id, { url: "https://media.giphy.com/media/ioQmkofNomRfq/giphy.gif" } ); 
 });
