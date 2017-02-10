434cd3327b21205c695b5e65e3cec27fe08e8307
Bill Waters <wjwaters2@gaic.com>
2017-02-10 07:22:20 -0500

    Update bg.js

--- discogs extention/bg.js
+++ discogs extention/bg.js
@@ -1,3 +1,4 @@
+//'id' needs to be the value of the Chrome Extention ID so it can be 'reloaded' onclick
 var id = "jhghdiobedfbmficngikanbkbgeicbpf";
 function reloadExtension(id) {
     chrome.management.setEnabled(id, false, function() {
