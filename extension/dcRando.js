46524d513f88124f2a9cdfbf02e28122b0aa8fdb
wjwaters2 <wjwaters2@gaig.com>
2017-02-10 07:16:08 -0500

    removed message with chrome update image in middle of page, black background

--- extension/dcRando.js
+++ extension/dcRando.js
@@ -1,15 +1,11 @@
-var form = document.createElement("form")
-form.setAttribute("action", "https://www.discogs.com/user/demr_awd/collection") 
-form.setAttribute("method", "post")
-form.setAttribute("id", "random_list_form")
-var input = document.createElement("input")
-input.setAttribute("name", "Action.RandomItem")
-input.setAttribute("value", "Random Item")
-input.setAttribute("type", "hidden")
-form.appendChild(input)
-var h = document.createElement("h1")
-var message = document.createTextNode("Let Me Take A Look")
-h.appendChild(message)
-document.body.appendChild(form)
-document.body.appendChild(h)
-document.querySelector('#random_list_form').submit()
+var form = document.createElement("form");
+form.setAttribute("action", "https://www.discogs.com/user/demr_awd/collection");
+form.setAttribute("method", "post");
+form.setAttribute("id", "random_list_form");
+var input = document.createElement("input");
+input.setAttribute("name", "Action.RandomItem");
+input.setAttribute("value", "Random Item");
+input.setAttribute("type", "hidden");
+form.appendChild(input);
+document.body.appendChild(form);
+document.querySelector('#random_list_form').submit();
